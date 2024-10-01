502034e750f373d2a4b4e075aabc42d42e9a402e
Dror Talmi <dror.gg.talmi@gmail.com>
2024-10-02 00:01:49 +0300

    spotless

--- src/main/java/frc/robot/subsystems/chooser/ChooserCommandsBuilder.java
+++ src/main/java/frc/robot/subsystems/chooser/ChooserCommandsBuilder.java
@@ -1,13 +1,10 @@
 package frc.robot.subsystems.chooser;
 
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 
 import java.util.function.DoubleSupplier;
-import java.util.function.Supplier;
 
 public class ChooserCommandsBuilder {
 
