713dd3a361160513f916b8f8258a794feccecb8f
Dror Talmi <dror.gg.talmi@gmail.com>
2024-10-01 20:55:00 +0300

    runcommand

--- src/main/java/frc/robot/subsystems/chooser/ChooserCommandsBuilder.java
+++ src/main/java/frc/robot/subsystems/chooser/ChooserCommandsBuilder.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
@@ -39,7 +40,7 @@ public class ChooserCommandsBuilder {
 	//@formatter:on
 
 	public Command stop() {
-		return new InstantCommand(chooser::stop, chooser).withName("Stop chooser");
+		return new RunCommand(chooser::stop, chooser).withName("Stop chooser");
 	}
 
 }
