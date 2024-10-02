5e915cb23696cda824794f66f69db924f45bc134
Dror Talmi <dror.gg.talmi@gmail.com>
2024-10-02 14:31:59 +0300

    usefunc

--- src/main/java/frc/robot/subsystems/chooser/Chooser.java
+++ src/main/java/frc/robot/subsystems/chooser/Chooser.java
@@ -51,7 +51,7 @@ public class Chooser extends GBSubsystem {
 	protected void subsystemPeriodic() {
 		updateInputs();
 		Logger.processInputs(chooserStuff.digitalInputLogPath(), digitalInputInputs);
-		Logger.recordOutput(chooserStuff.digitalInputLogPath() + "isNoteIn", digitalInputInputs.debouncedValue);
+		Logger.recordOutput(isNoteIn() + "isNoteIn", digitalInputInputs.debouncedValue);
 	}
 
 }
