136ba098430541cc3193f93bce74c3fe3442bc23
Yotam7 <76944434+Yotam7@users.noreply.github.com>
2024-10-02 17:39:38 +0300

    Yotam- elevator roller

--- src/main/java/frc/robot/constants/IDs.java
+++ src/main/java/frc/robot/constants/IDs.java
@@ -1,6 +1,7 @@
 package frc.robot.constants;
 
 import edu.wpi.first.wpilibj.PowerDistribution;
+import frc.robot.hardware.motor.sparkmax.SparkMaxDeviceID;
 import frc.utils.battery.PowerDistributionDeviceID;
 
 public class IDs {
@@ -9,5 +10,7 @@ public class IDs {
 		20,
 		PowerDistribution.ModuleType.kRev
 	);
+
+	public static final SparkMaxDeviceID ELEVATOR_ROLLER = new SparkMaxDeviceID(0); // ! TODO replace with real ID
 
 }
